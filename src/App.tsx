f301ff2c416acdb42f2862ad45791c8f59578361
sarfaraz187 <sarfarazmohammed187@gmail.com>
2023-10-02 22:26:42 +0200

    build process of tailwind css implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home></Home>
     </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
-import './App.css';
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-lg">
+      {' '}
+      <h1 className="text-2xl font-bold text-gray-800">Centered Container</h1>
+      <p className="text-gray-600">This container is centered on the page.</p>
+      <button className="bg-indigo-500 ...">Save changes</button>
+    </div>
+  );
 }
 
 export default Home;
